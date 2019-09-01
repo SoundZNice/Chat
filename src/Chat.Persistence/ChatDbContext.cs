9ade4b006ff8db289189f07c6b20149e50dc422c
Smilers <monmorti@gmail.com>
2019-09-01 03:21:47 +0300

    db context

---
+++ src/Chat.Persistence/ChatDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Chat.Application.Interfaces;
+using Chat.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chat.Persistence
+{
+    public class ChatDbContext : DbContext, IChatDbContext
+    {
+        public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options) { }
+        public Task SaveAsync(CancellationToken cancellation)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
