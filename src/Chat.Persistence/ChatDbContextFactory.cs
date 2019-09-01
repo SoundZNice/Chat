9c08381e451ff9146ac3542c2c23b8c83d404f46
Smilers <monmorti@gmail.com>
2019-09-01 03:20:44 +0300

    ChatDbContext factory

---
+++ src/Chat.Persistence/ChatDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Chat.Persistence.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chat.Persistence
+{
+    public class ChatDbContextFactory : DesignTimeDbContextFactoryBase<ChatDbContext>
+    {
+        protected override ChatDbContext CreateNewInstance(DbContextOptions<ChatDbContext> options)
+        {
+            return new ChatDbContext(options);
+        }
+    }
+}
