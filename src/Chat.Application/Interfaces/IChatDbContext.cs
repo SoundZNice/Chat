0ef49ebfe71272a4d6f7f390e1e8420c02e903cc
Smilers <monmorti@gmail.com>
2019-09-01 03:20:54 +0300

    initial

---
+++ src/Chat.Application/Interfaces/IChatDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chat.Application.Interfaces
+{
+    public interface IChatDbContext
+    {
+        Task SaveAsync(CancellationToken cancellation);
+    }
+}
