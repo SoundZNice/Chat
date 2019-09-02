bc8eacf50fca57cfb7af502b57f34ef87784d65a
Smilers <monmorti@gmail.com>
2019-09-02 23:51:49 +0300

    initial  commit

---
+++ src/Chat.Application/Account/LoginUserCommand/LoginUserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Chat.Application.Account.LoginUserCommand
+{
+    public class LoginUserViewModel
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
