34aaa8601a394bb31aaf62d9fa4b344a9df0cc0a
Smilers <monmorti@gmail.com>
2019-09-03 01:24:42 +0300

    username to email

--- src/Chat.Application/Account/LoginCommand/LoginCommand.cs
+++ src/Chat.Application/Account/LoginCommand/LoginCommand.cs
@@ -4,6 +4,7 @@ namespace Chat.Application.Account.LoginUserCommand
 {
     public class LoginCommand : IRequest<LoginCommandResult>
     {
+        public string Email { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
     }
