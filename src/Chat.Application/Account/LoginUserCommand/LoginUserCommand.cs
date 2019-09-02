5ea799dd04053cb807ddb7e886567d41ee164056
Smilers <monmorti@gmail.com>
2019-09-02 23:52:06 +0300

    initial

---
+++ src/Chat.Application/Account/LoginUserCommand/LoginUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Chat.Application.Account.LoginUserCommand
+{
+    public class LoginUserCommand : IRequest<LoginUserViewModel>
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
