8a61b36457696c76be4034134381b7e138ac5d01
Smilers <monmorti@gmail.com>
2019-09-05 23:50:30 +0300

    secure guid and comments

--- src/Chat.Application/Interfaces/Authentication/ISecurityService.cs
+++ src/Chat.Application/Interfaces/Authentication/ISecurityService.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace Chat.Application.Interfaces.Authentication
 {
     public interface ISecurityService
     {
+        // Create converted to base64
+        // string hash which is stored in database
         string GetSha256Hash(string input);
+
+        // Generate unique id for jwt id
+        Guid CreateCryptographicallySecureGuid();
     }
 }
