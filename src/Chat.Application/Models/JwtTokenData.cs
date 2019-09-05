68b58a63632eb42d41bdf40726597ea5c80ca5fc
Smilers <monmorti@gmail.com>
2019-09-05 23:34:04 +0300

    jwtTokenModel

---
+++ src/Chat.Application/Models/JwtTokenData.cs
@@ -0,0 +1,8 @@
+namespace Chat.Application.Models
+{
+    public class JwtTokenData
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
